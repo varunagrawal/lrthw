ea120ec24882fc4af9e2e36a4a6981a541e7c561
Varun Agrawal <varagrawal@gmail.com>
2015-01-05 15:38:43 +0530

    ex49b
    Added Parser class

--- projects/ex48/lib/ex48/parser.rb
+++ projects/ex48/lib/ex48/parser.rb
@@ -15,75 +15,80 @@ class Sentence
   attr_reader :object
 end
 
-def peek(word_list)
-  if word_list
-    word = word_list[0]
-    return word[0]
-  else
-    return nil
-  end
-end
 
-def match(word_list, expecting)
-  if word_list
-    word = word_list.shift
-    
-    if word[0] == expecting
-      return word
+class Parser
+
+  def peek(word_list)
+    if word_list
+      word = word_list[0]
+      return word[0]
     else
       return nil
     end
-  else
-    return nil
   end
-end
 
-def skip_words(word_list, word_type)
-  while peek(word_list) == word_type
-    match(word_list, word_type)
+  def match(word_list, expecting)
+    if word_list
+      word = word_list.shift
+      
+      if word[0] == expecting
+        return word
+      else
+        return nil
+      end
+    else
+      return nil
+    end
   end
-end
 
-def parse_verb(word_list)
-  skip_words(word_list, 'stop')
-  
-  if peek(word_list) == 'verb'
-    return match(word_list, 'verb')
-  else
-    raise ParserError.new("Expected a verb next.")
+  def skip_words(word_list, word_type)
+    while peek(word_list) == word_type
+      match(word_list, word_type)
+    end
   end
-end
 
-def parse_object(word_list)
-  skip_words(word_list, 'stop')
-  next_word = peek(word_list)
-  
-  if next_word == 'noun'
-    return match(word_list, 'noun')
-  elsif next_word == 'direction'
-    return match(word_list, 'direction')
-  else
-    raise ParserError.new("Expected a noun or direction next.")
+  def parse_verb(word_list)
+    skip_words(word_list, 'stop')
+    
+    if peek(word_list) == 'verb'
+      return match(word_list, 'verb')
+    else
+      raise ParserError.new("Expected a verb next.")
+    end
   end
-end
 
-def parse_subject(word_list)
-  skip_words(word_list, 'stop')
-  next_word = peek(word_list)
-  
-  if next_word == 'noun'
-    return match(word_list, 'noun')
-  elsif next_word == 'verb'
-    return ['noun', 'player']
-  else
-    raise ParserError.new("Expected a verb next.")
+  def parse_object(word_list)
+    skip_words(word_list, 'stop')
+    next_word = peek(word_list)
+    
+    if next_word == 'noun'
+      return match(word_list, 'noun')
+    elsif next_word == 'direction'
+      return match(word_list, 'direction')
+    else
+      raise ParserError.new("Expected a noun or direction next.")
+    end
   end
-end
 
-def parse_sentence(word_list)
-  subj = parse_subject(word_list)
-  verb = parse_verb(word_list)
-  obj = parse_object(word_list)
-  
-  return Sentence.new(subj, verb, obj)
+  def parse_subject(word_list)
+    skip_words(word_list, 'stop')
+    next_word = peek(word_list)
+    
+    if next_word == 'noun'
+      return match(word_list, 'noun')
+    elsif next_word == 'verb'
+      return ['noun', 'player']
+    else
+      raise ParserError.new("Expected a verb next.")
+    end
+  end
+
+  def parse_sentence(word_list)
+    subj = parse_subject(word_list)
+    verb = parse_verb(word_list)
+    obj = parse_object(word_list)
+    
+    return Sentence.new(subj, verb, obj)
+  end
+
 end
--- projects/ex48/tests/test_parser.rb
+++ projects/ex48/tests/test_parser.rb
@@ -6,52 +6,52 @@ class TestGame < Minitest::Test
   def test_peek()
     word_list = [['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey']]
     
-    assert_equal(peek(nil), nil)
-    assert_equal(peek(word_list), 'noun')
+    assert_equal(Parser.new.peek(nil), nil)
+    assert_equal(Parser.new.peek(word_list), 'noun')
   end
   
   def test_match()
     word_list = [['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey']]
     
-    assert_equal(match(nil, 'noun'), nil)
-    assert_equal(match(word_list, 'noun'), ['noun', 'bear'])
-    assert_equal(match(word_list, 'noun'), nil) # the word_list gets modified due to the shift
+    assert_equal(Parser.new.match(nil, 'noun'), nil)
+    assert_equal(Parser.new.match(word_list, 'noun'), ['noun', 'bear'])
+    assert_equal(Parser.new.match(word_list, 'noun'), nil) # the word_list gets modified due to the shift
   end
   
   def test_skip()
     word_list = [['stop', 'from'], ['stop', 'the'], ['noun', 'west']]
     
-    assert_equal(skip_words(word_list, 'stop'), nil)
-    assert_equal(skip_words([['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey']], 'noun'), nil)
+    assert_equal(Parser.new.skip_words(word_list, 'stop'), nil)
+    assert_equal(Parser.new.skip_words([['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey']], 'noun'), nil)
   end
   
   def test_subject()    
-    assert_equal(parse_subject([['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey']]), ['noun', 'bear'])
-    assert_equal(parse_subject([['verb', 'eat'], ['noun', 'honey']]), ['noun', 'player'])
+    assert_equal(Parser.new.parse_subject([['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey']]), ['noun', 'bear'])
+    assert_equal(Parser.new.parse_subject([['verb', 'eat'], ['noun', 'honey']]), ['noun', 'player'])
   end
   
   def test_verb()
     
     assert_raises ParserError do 
-      parse_verb([['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey']]) 
+      Parser.new.parse_verb([['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey']]) 
     end
     
-    assert_equal(parse_verb([['verb', 'eat'], ['noun', 'honey']]), ['verb', 'eat'])
+    assert_equal(Parser.new.parse_verb([['verb', 'eat'], ['noun', 'honey']]), ['verb', 'eat'])
   end
   
   def test_object()
-    assert_equal(parse_object([['stop', 'the'], ['noun', 'bear']]), ['noun', 'bear'])
-    assert_equal(parse_object([['stop', 'the'], ['direction', 'south']]), ['direction', 'south'])
+    assert_equal(Parser.new.parse_object([['stop', 'the'], ['noun', 'bear']]), ['noun', 'bear'])
+    assert_equal(Parser.new.parse_object([['stop', 'the'], ['direction', 'south']]), ['direction', 'south'])
     
     assert_raises ParserError do
-      parse_object([['verb', 'go']])
+      Parser.new.parse_object([['verb', 'go']])
     end
   end 
   
   def test_sentence()
     word_list = [['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey']]
     
-    sentence = parse_sentence(word_list)
+    sentence = Parser.new.parse_sentence(word_list)
     sent = Sentence.new(['noun', 'bear'], ['verb', 'eat'], ['noun', 'honey'])
     
     assert_equal(sent.subject, sentence.subject)
