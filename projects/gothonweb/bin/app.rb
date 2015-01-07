3c1f1a9cc61f61ce88c3390f078492964bbb6622
Varun Agrawal <varagrawal@gmail.com>
2015-01-08 00:56:34 +0530

    ex51

--- projects/gothonweb/bin/app.rb
+++ projects/gothonweb/bin/app.rb
@@ -10,6 +10,16 @@ get '/' do
 end
 
 get '/hello/' do
+    erb :hello_form
+end
+
+post '/hello/' do
     greeting = params[:greeting] || "Hi There"
-    erb :index, :locals => {'greeting' => greeting}
+    name = params[:name] || "Nobody"
+
+    File.open('uploads/' + params[:file][:filename], 'w') do |f|
+        f.write(params[:file][:tempfile].read)
+    end
+
+    erb :index, :locals => {'greeting' => greeting, 'name' => name}
 end
